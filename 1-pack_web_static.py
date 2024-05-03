87e5ecac2b89cc1ddf4ced2d3b781d019f1ad2eb
djshampz904 <martin.kimani@strathmore.edu>
2024-05-03 19:46:55 +0300

    airbnb

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
+# Fabfile
 import os.path
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
+    """Create a tar gzipped archive."""
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
                                                          dt.month,
