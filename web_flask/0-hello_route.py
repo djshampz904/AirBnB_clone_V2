4405db80743478c98144e1e76afdb9edb706ff2e
djshampz904 <martin.kimani@strathmore.edu>
2024-05-17 16:31:13 +0300

    flask 1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Display Hello HBNB! """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0')
